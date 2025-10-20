a590f39accd94e231132f2b1bc714e37dd9709f9
merialea <merialea5@gmail.com>
2025-10-20 13:24:37 +0300

    complete lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,25 @@
 // create your App component here
+import React,{useEffect,useState} from 'react';
+
+function App() {
+    const [dogImage, setDogImage] = useState("");
+
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then(response => response.json())
+        .then(data => setDogImage(data.message))
+        
+    }, []);
+
+    if (!dogImage) return <p>Loading...</p>;
+
+    return (
+        <div>
+            <img src={dogImage} alt="A Random Dog" />
+        </div>
+    );
+
+
+}
+export default App;
+
